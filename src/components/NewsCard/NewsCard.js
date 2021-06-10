bbeb0734fc6334b4fd94b8cc5c20ef3874358cc8
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-06-10 13:48:43 -0400

    feat: add NewsCard component and fetch top news

---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NewsCard({ news }) {
+  return <div>{news.title}</div>;
+}
+
+export default NewsCard;
--- src/components/TopNews/TopNews.js
+++ src/components/TopNews/TopNews.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useFetchNews from '../../../hooks/useFetchNews';
+import NewsCard from '../NewsCard/NewsCard';
 
 function TopNews() {
-  return <div>Hello from top news component</div>;
+  const topNews = useFetchNews(
+    'https://newsapi.org/v2/top-headlines?country=us&apiKey=de672c5dc9894971ba8967feb68e7431'
+  );
+
+  return (
+    <>
+      {topNews ? (
+        <div>
+          {topNews.map((news) => (
+            <NewsCard news={news} />
+          ))}
+        </div>
+      ) : (
+        <p>loading...</p>
+      )}
+    </>
+  );
 }
 
 export default TopNews;
