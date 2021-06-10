6f196983d5e6f3cf7bdd745ed1e05240f836173a
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-06-10 17:19:56 -0400

    feat: make dashboard a protected page

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,30 +1,50 @@
 /** @jsxImportSource theme-ui */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import SearchNews from '../../src/components/SearchNews/SearchNews';
 import TopNews from '../../src/components/TopNews/TopNews';
 import RecentlyViewed from '../../src/components/RecentlyViewed/RecentlyViewed';
 import DashNav from '../../src/components/DashNav/DashNav';
-import { Divider } from '@theme-ui/components';
+import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+import { Spinner } from '@theme-ui/components';
 
 function Dashboard() {
+  const { isAuthenticated } = useSelector(
+    (state) => state.authenticationReducer
+  );
+  console.log(isAuthenticated);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push('/');
+    }
+  }, []);
+
   return (
     <>
       {/* sort by */}
-      <DashNav />
-      <div sx={{ display: 'flex', flexDirection: 'column' }}>
-        <div sx={{ display: 'flex' }}>
-          <div sx={{ flex: 2 }}>
-            <SearchNews />
+      {isAuthenticated ? (
+        <>
+          <DashNav />
+          <div sx={{ display: 'flex', flexDirection: 'column' }}>
+            <div sx={{ display: 'flex' }}>
+              <div sx={{ flex: 2 }}>
+                <SearchNews />
+              </div>
+              <div sx={{ flex: 1 }}>
+                <TopNews />
+              </div>
+            </div>
+            <div>
+              <RecentlyViewed />
+            </div>
           </div>
-          <div sx={{ flex: 1 }}>
-            <TopNews />
-          </div>
-        </div>
-        <div>
-          <RecentlyViewed />
-        </div>
-      </div>
+        </>
+      ) : (
+        <Spinner />
+      )}
     </>
   );
 }
