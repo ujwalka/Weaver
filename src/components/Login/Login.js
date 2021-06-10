6f53ff555762fa2de65347d628d4193e9e1849b3
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-06-10 07:29:29 -0400

    feat: add login page without css

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,44 @@
-import React from 'react';
+/** @jsxImportSource theme-ui */
 
 function Login() {
   return (
-    <div>
-      {/* login form */}
-      {/* username */}
-      {/* pw */}
-      {/* submit */}
-      <div className=''>Hello from login</div>
+    <div className='form'>
+      <form
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          bg: 'white',
+        }}
+        action='submit'
+        // onSubmit={}
+      >
+        <h1>Login</h1>
+
+        <label htmlFor='email'>Email</label>
+        <input
+          type='email'
+          name='email'
+          id='name'
+          // value={}
+          // onChange={}
+          placeholder='jdoe@gmail.com'
+          required
+        />
+        <label htmlFor='password'>Password</label>
+        <input
+          type='password'
+          name='password'
+          id='password'
+          // value={}
+          // onChange={}
+          required
+        />
+
+        <div sx={{ paddingTop: '0.5rem' }}>
+          <button type='submit'>Register</button>
+        </div>
+      </form>
     </div>
   );
 }
