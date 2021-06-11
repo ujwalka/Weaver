9ae89688c784b23269e8acc14f081c39a9252f2e
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-06-11 09:57:02 -0400

    fix: add spinner to make the vercel build pass

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Spinner } from 'theme-ui';
 
 function NewsArticle() {
   // @ts-ignore
   const { currentArticle } = useSelector((state) => state.newsReducer);
   return (
     <>
-      <div className=''>{currentArticle.title}</div>
+      {currentArticle ? (
+        <div className=''>{currentArticle.title}</div>
+      ) : (
+        <Spinner />
+      )}
     </>
   );
 }
