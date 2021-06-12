96ac49d15f6a98ef6fc1c3947b43106d33ef07ec
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-06-11 20:56:09 -0400

    note: incomplete nestcard, eod push

--- src/components/NestCard/NestCard.js
+++ src/components/NestCard/NestCard.js
@@ -1,10 +1,39 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Card, Box, Heading, Text } from 'theme-ui';
+import NewsCard from '../NewsCard/NewsCard';
 function NestCard({ nest }) {
+  const [articles, setArticles] = useState(null);
+  useEffect(() => {
+    if (nest && nest.articles) {
+      // const articleList = nest.articles.map((article) => JSON.parse(article));
+      // get articles
+      // display last 3
+      console.log(nest.articles);
+      // setArticles(articleList);
+    }
+  }, [articles]);
+  console.log(nest);
   return (
     <div>
-      {/* strawlist */}
-      {nest.description}
+      <>
+        <Card
+          sx={{
+            display: 'flex',
+          }}
+        >
+          <Box sx={{ flex: 4 }}>
+            <Heading as='h3' mb={2}>
+              {nest.description}
+            </Heading>
+          </Box>
+
+          {nest.notes ? <Text> {nest.notes}</Text> : null}
+          {articles
+            ? articles.map((article) => <p>{article}</p>})
+            : null}
+        </Card>
+      </>
+      {/* notes, articles */}
     </div>
   );
 }
