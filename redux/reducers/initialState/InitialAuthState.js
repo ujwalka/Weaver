ae1d8abf0dd81e040294e391cbfdb5f9f70ebc2f
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-06-13 18:41:16 -0400

    fix: resolve vercel module not found first

--- redux/reducers/initialState/InitialAuthState.js
+++ redux/reducers/initialState/InitialAuthState.js
@@ -2,3 +2,5 @@ export const initialAuthState = {
   isAuthenticated: false,
   user: null,
 };
+
+// resolve vercel ModuleNotFoundError: Module not found: Error: Can't resolve './initialState/initialAuthState' in '/vercel/path0/redux/reducers'
